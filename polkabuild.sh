35a82c5d6d866f71fe0618d4ca606611910d14ac
SystematicError <systematicerror@users.noreply.github.com>
2022-06-12 06:59:19 +0000

    Sync mpv uosc script

--- polkabuild.sh
+++ polkabuild.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-printf "This is just a test\n"
+if [ -z "$DOTFILES_DIR" ]; then
+    printf "\u001b[1m\u001b[31m"
+    printf "DOTFILES_DIR environment variable was not passed!"
+    printf "\u001b[0m\n"
+fi
+
+# Not technically a "build" but adding it anyway
+# Update mpv uosc script
+curl https://raw.githubusercontent.com/darsain/uosc/master/uosc.lua > mpv/scripts/uosc.lua
 
