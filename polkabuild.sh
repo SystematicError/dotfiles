bad48812f312b76ed7a1c1abbfb6bdd5b9a677f7
SystematicError <systematicerror@users.noreply.github.com>
2022-06-12 08:59:58 +0000

    Add build scripts for lua bindings

--- polkabuild.sh
+++ polkabuild.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-if [ -z "$DOTFILES_DIR" ]; then
-    printf "\u001b[1m\u001b[31m"
-    printf "DOTFILES_DIR environment variable was not passed!"
-    printf "\u001b[0m\n"
-fi
-
 # Not technically a "build" but adding it anyway
-# Update mpv uosc script
+echo "Syncing mpv scripts..."
 curl https://raw.githubusercontent.com/darsain/uosc/master/uosc.lua > mpv/scripts/uosc.lua
+
+echo
+
+echo "Building lua pam bindings..."
+git clone https://github.com/RMTT/lua-pam.git /tmp/lua-pam
+cmake /tmp/lua-pam -B /tmp/lua-pam/build
+make -C /tmp/lua-pam/build
+mv /tmp/lua-pam/build/liblua_pam.so awesome/module/
+rm -rf /tmp/lua-pam
+
+echo
+
+echo "Building lua pulse bindings..."
+git clone https://github.com/sclu1034/lua-libpulse-glib.git /tmp/lua-libpulse-glib
+make -C /tmp/lua-libpulse-glib
+mv /tmp/lua-libpulse-glib/out/lua_libpulse_glib.so awesome/module
+rm -rf /tmp/lua-libpulse-glib
 
