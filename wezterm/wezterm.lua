62bff81d50a06aff34bf32cd4566bef79d165b8f
SystematicError <systematicerror@users.noreply.github.com>
2022-08-05 08:22:58 +0000

    Fix font name

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,7 +10,7 @@ return {
     hide_tab_bar_if_only_one_tab = true,
 
     font = wezterm.font_with_fallback {
-        "JetBrains Mono Nerd Font",
+        "JetBrainsMono Nerd Font",
         "Twemoji"
     },
 
