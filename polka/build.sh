89832723733d15d5a8e419c1ec57cbfb91064200
SystematicError <systematicerror@users.noreply.github.com>
2022-08-15 13:57:18 +0000

    Temporarily disable build scripts for pulse bindings

--- polka/build.sh
+++ polka/build.sh
@@ -7,11 +7,11 @@ make -C /tmp/lua-pam/build
 mv /tmp/lua-pam/build/liblua_pam.so awesome/module/
 rm -rf /tmp/lua-pam
 
-echo
-
-echo "Building lua pulse bindings..."
-git clone https://github.com/sclu1034/lua-libpulse-glib.git /tmp/lua-libpulse-glib
-make -C /tmp/lua-libpulse-glib
-mv /tmp/lua-libpulse-glib/out/lua_libpulse_glib.so awesome/module
-rm -rf /tmp/lua-libpulse-glib
+# echo
+# 
+# echo "Building lua pulse bindings..."
+# git clone https://github.com/sclu1034/lua-libpulse-glib.git /tmp/lua-libpulse-glib
+# make -C /tmp/lua-libpulse-glib
+# mv /tmp/lua-libpulse-glib/out/lua_libpulse_glib.so awesome/module
+# rm -rf /tmp/lua-libpulse-glib
 
