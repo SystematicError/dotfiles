3e793362b35d41a3f74e36f699e346bb73e9f960
SystematicError <systematicerror@users.noreply.github.com>
2022-08-28 10:06:27 +0000

    Respect application size hints

--- awesome/rules.lua
+++ awesome/rules.lua
@@ -2,11 +2,6 @@ local ruled = require "ruled"
 
 ruled.client.connect_signal("request::rules", function()
     ruled.client.append_rule {
-        rule = {},
-        properties = {size_hints_honor = false}
-    }
-
-    ruled.client.append_rule {
         rule = {
             class = "firefox",
             name = "Picture-in-Picture"
