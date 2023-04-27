721d3799e7e44b1a931f4413ac7f034d21b5fd64
SystematicError <systematicerror@users.noreply.github.com>
2023-04-27 08:20:26 +0000

    Inherit background and foreground colours when lazy drawing

--- awesome/module/slide.lua
+++ awesome/module/slide.lua
@@ -1,8 +1,23 @@
 local wibox = require "wibox"
+local cairo = require("lgi").cairo
 
 local rubato = require "module.rubato"
 
 local module = {path = {}}
+
+-- Patched version of wibox.widget.draw_to_image_surface to accept fg and bg variables
+local function draw_to_image_surface(wdg, width, height, format, context, bg, fg)
+    local img = cairo.ImageSurface(format or cairo.Format.ARGB32, width, height)
+    local cr = cairo.Context(img)
+
+    cr:set_source(bg)
+    cr:paint()
+
+    cr:set_source(fg)
+
+    wibox.widget.draw_to_cairo_context(wdg, cr, width, height, context)
+    return img
+end
 
 function module.path.top_down(popup, reverse)
     return {
@@ -18,7 +33,7 @@ local function animate(popup, path, on_start, on_end)
     local animation = rubato.timed {
         pos = path.from,
 
-        duration = 0.25,
+        duration = 0.3,
         easing = rubato.easing.quadratic,
         awestore_compat = true,
 
@@ -45,7 +60,19 @@ function module.toggle(popup, path_func, on_end)
         border_width = popup.border_width,
         border_color = popup.border_color,
 
-        widget = wibox.widget.imagebox(wibox.widget.draw_to_image_surface(popup.widget, popup.width, popup.height), false)
+        widget = {
+            image = draw_to_image_surface(
+                popup.widget,
+                popup.width,
+                popup.height,
+                nil,
+                nil,
+                popup._drawable.background_color,
+                popup._drawable.foreground_color
+            ),
+            resize = false,
+            widget = wibox.widget.imagebox
+        }
     }
 
     local path = path_func(lazy_popup, popup.visible)
