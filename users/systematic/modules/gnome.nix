0ff41ee517ca4a25cd7e823f632b95b05d2f0387
SystematicError <systematicerror@users.noreply.github.com>
2023-12-25 17:08:40 +0000

    Fix adw-gtk3 using light theme

--- users/systematic/modules/gnome.nix
+++ users/systematic/modules/gnome.nix
@@ -55,6 +55,10 @@
     };
 
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
   };
 
   services.easyeffects = {
