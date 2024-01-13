e62d1023720d88462846af67571406fcd61d9b4c
SystematicError <systematicerror@users.noreply.github.com>
2024-01-13 18:20:54 +0000

    feat: tweak window appearance

--- users/systematic/modules/hyprland.nix
+++ users/systematic/modules/hyprland.nix
@@ -44,14 +44,16 @@
       ];
 
       general = {
-        border_size = 0;
         gaps_in = 8;
         gaps_out = 16;
         resize_on_border = true;
+        "col.active_border" = "rgb(252525)";
+        "col.inactive_border" = "rgb(252525)";
       };
 
       decoration = {
         rounding = 16;
+        "col.shadow" = "rgba(00000000)";
       };
 
       master = {
