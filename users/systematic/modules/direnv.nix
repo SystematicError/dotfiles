abb39b02e55ca0caea7e4c7ec58981303da1c6ed
SystematicError <systematicerror@users.noreply.github.com>
2024-02-03 10:51:48 +0000

    feat: wrap direnv

--- users/systematic/modules/direnv.nix
+++ users/systematic/modules/direnv.nix
@@ -1,8 +1,14 @@
-{...}: {
-  home.sessionVariables.DIRENV_LOG_FORMAT = "";
-
+{pkgs, ...}: {
   programs.direnv = {
     enable = true;
+
+    package = pkgs.symlinkJoin {
+      name = pkgs.direnv.name;
+      paths = [pkgs.direnv];
+      nativeBuildInputs = [pkgs.makeWrapper];
+      postBuild = ''wrapProgram "$out/bin/direnv" --set DIRENV_LOG_FORMAT ""'';
+    };
+
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
