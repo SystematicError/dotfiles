9137011e18ab35f1964ba754fb42359335c89acf
SystematicError <systematicerror@users.noreply.github.com>
2025-02-21 10:05:54 +0000

    fix: update firefox extension configuration schema

--- users/systematic/modules/firefox.nix
+++ users/systematic/modules/firefox.nix
@@ -7,7 +7,7 @@
     enable = true;
 
     profiles.frosted = {
-      extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+      extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
         darkreader
         return-youtube-dislikes
         sponsorblock
